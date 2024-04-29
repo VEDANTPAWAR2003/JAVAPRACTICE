3356939675acd86609f9ef44dcec84513f2c3e73
VEDANTPAWAR2003 <155801883+VEDANTPAWAR2003@users.noreply.github.com>
2024-04-29 12:04:11 +0530

    Add files via upload

---
+++ pr14.java
@@ -0,0 +1,17 @@
+class Aminal{
+ public static void main(String ...args)
+ {
+ 
+  try{
+   System.out.println(args[0]);
+   }
+   catch(Exception e)
+       {
+          System.out.println("Heloo");
+       }
+   finally{
+      System.out.println("printit");
+  }
+}
+}
+
